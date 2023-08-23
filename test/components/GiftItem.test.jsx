78637fe0271b7ac238389ef80c8a6a3ef14d900f
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-08-22 19:31:31 -0500

    Added new test in the GiftItem component. Also snapshots updated

--- test/components/GiftItem.test.jsx
+++ test/components/GiftItem.test.jsx
@@ -1,17 +1,33 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import GiftItem from "../../src/components/GiftItem";
 
 /* eslint-disable no-undef */
-describe('Testing in GiftItem', () => {
-  test('should match with snapshot', () => {
+describe("Testing in GiftItem", () => {
+  const title = "This is a title";
+  const url = "https://url.com/";
 
-    const title= 'This is a title';
-    const url = 'https://url.com';
+  test("should match with snapshot", () => {
+    const { container } = render(<GiftItem title={title} url={url} />);
 
-    const {container} = render(<GiftItem title={title} url={url}/>)
+    expect(container).toMatchSnapshot();
+  });
 
-    expect(container).toMatchSnapshot()
-    
-  })
-  
-})
+  test("should show the image with the url and ALT indicated", () => {
+    render(<GiftItem title={title} url={url} />);
+
+    //forma 1
+    expect(screen.getByRole("img").src).toBe(url);
+    expect(screen.getByRole("img").alt).toBe(title);
+
+    //forma 2 evitando repedicion de codigo
+    const { src, alt } = screen.getByRole("img");
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+  });
+
+  test("should show the title as a text in the component", () => {
+    render(<GiftItem title={title} url={url} />);
+
+    expect(screen.getByText(title)).toBeTruthy();
+  });
+});
